75b24e2719441157008a79c202cce5c8195f77e7
zhuchella <nate9314@mail.ru>
2019-10-25 12:09:04 +0300

    test

--- test_for_giyhub.py
+++ test_for_giyhub.py
@@ -5,10 +5,3 @@ from selenium.webdriver.common.by import By
 import time
 
 
-class SampleTest(unittest.TestCase):
-
-    def setUp(self):
-        self.driver = webdriver.Chrome()
-        self.driver.maximize_window()
-        self.driver.implicitly_wait(30)
-        self.driver.set_page_load_timeout(30)
